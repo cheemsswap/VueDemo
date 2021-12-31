207f3cb12f2c1f37fb5cd550a4380ca8fe37dd51
cheemsswap <291221550@qq.com>
2021-12-31 18:50:07 +0800

    12-31日 完成demo46 ，学习了vue的props

---
+++ Demo46_Vue props/demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Student
+      v-for="s in Students"
+      :key="s.name"
+      :name="s.name"
+      :sex="s.sex"
+      :age="s.age"
+    ></Student>
+  </div>
+</template>
+
+<script>
+import Student from "./components/Student.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      Students: [
+        { name: "张三", sex: "男", age: 101 },
+        { name: "李四", sex: "男" },
+        { name: "王五", sex: "男", age: 50 },
+      ],
+    };
+  },
+  components: { Student },
+};
+</script>
+
+<style>
+</style>
---
+++ Demo46_Vue props/demo/src/components/Student.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>学生姓名:{{ name }}</h2>
+    <h2>学生性别:{{ sex }}</h2>
+    <h2>学生年龄:{{ age }}</h2>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  //   props: ["name", "sex", "age"],
+  props: {
+    name: {
+      typeof: String,
+      required: true,
+    },
+    sex: {
+      typeof: String,
+      required: true,
+    },
+    age: {
+      typeof: Number,
+      default: 18,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
