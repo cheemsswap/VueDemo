82c73ba2f7cbbf03d99a0902ecf1e06eb189d72e
cheemsswap <291221550@qq.com>
2022-01-01 14:42:24 +0800

    1-1日 中午完成案例 demo48 了解了vue style里面的scoped 私有化样式

---
+++ Demo48_Vue style-scoped 私有化样式/demo/src/components/School.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h2>学校名称:{{ name }}</h2>
+    <h2>学校地址:{{ address }}</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      name: "家里蹲大学",
+      address: "家里",
+    };
+  },
+};
+</script> 
+<style lang="css" scoped>
+.demo {
+  background-color: bisque;
+}
+</style>
---
+++ Demo48_Vue style-scoped 私有化样式/demo/src/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo">
+    <h2>学校姓名:{{ name }}</h2>
+    <h2>学生年龄:{{ age }}</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      name: "张三",
+      age: 18,
+    };
+  },
+};
+</script> 
+<style lang="css" scoped>
+.demo {
+  background-color: aquamarine;
+}
+</style>
