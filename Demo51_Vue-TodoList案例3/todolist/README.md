98abed87da4c66c87d4edf2acbd62dfe0d2ffaf3
cheemsswap <291221550@qq.com>
2022-01-03 22:59:35 +0800

    1-3日完成demo51  观看视频 优化了todolist案例 将隐藏的一些vue不能监听的v-model在props里面一些改进了

---
+++ Demo51_Vue-TodoList案例3/todolist/README.md
@@ -0,0 +1,4 @@
+导入 nanoid 库
+npm i nanoid
+
+Array  reduce 的使用 详情查看 todo-footer代码
---
+++ Demo51_Vue-TodoList案例3/todolist/src/components/todo-list/com/todo-main.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="todo-main">
+    <todo-main-item
+      v-for="work of works"
+      :key="work.id"
+      :work="work"
+      :updataWorkDeoe="updataWorkDeoe"
+      :deleteWork="deleteWork"
+    ></todo-main-item>
+  </ul>
+</template>
+
+<script>
+import todoMainItem from "./todo-main-item.vue";
+export default {
+  components: { todoMainItem },
+  props: {
+    works: Array,
+    //提供给header 添加数据
+    updataWorkDeoe: Function,
+    //删除指定id的work
+    deleteWork: Function,
+  },
+};
+</script>
+
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
