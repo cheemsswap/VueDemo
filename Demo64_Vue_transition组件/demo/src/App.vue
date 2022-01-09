fed48c0cccf8729e658cc1aacfaad831f2c1309e
cheemsswap <291221550@qq.com>
2022-01-09 11:54:10 +0800

    1月9日 完成了demo64-65 了解了vue的动画过渡 transition 组件里面的2个css类 .v-enter-active 和 v-leave-active

---
+++ Demo64_Vue_transition组件/demo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <demo />
+  </div>
+</template>
+
+<script>
+import Demo from "./components/demo.vue";
+export default {
+  name: "App",
+  components: { Demo },
+};
+</script>
+
+<style>
+body {
+  width: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ Demo64_Vue_transition组件/demo/src/components/demo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button @click="fun">点我隐藏/显示</button>
+    <transition>
+      <h2 v-show="isShow"></h2>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {
+    fun() {
+      this.isShow = !this.isShow;
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+h2 {
+  width: 100%;
+  height: 80px;
+  background-color: orange;
+}
+.v-enter-active {
+  animation: in 2s;
+}
+.v-leave-active {
+  animation: in 2s reverse;
+}
+@keyframes in {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+</style>
