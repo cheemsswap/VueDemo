633c2e278fc18ccb96b56e4a179d4c77d1ae0f07
cheemsswap <291221550@qq.com>
2022-01-18 18:27:57 +0800

    1月18日晚上完成了demo96 了解了Vue-router里面的keep-active 标签

---
+++ Demo96_Vue-router-缓存路由组件/demo/README.md
@@ -0,0 +1,4 @@
+<keep-alive> 保持活跃的 为路由组件加以标识不会被摧毁
+
+<keep-alive include="ContactUs">
+注意要指定某个组件不被摧毁保存活跃 ：路由组件必须要有name
---
+++ Demo96_Vue-router-缓存路由组件/demo/src/view/ContactUs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="con of ContactUsList" :key="con.title">
+        <router-link
+          :to="{
+            name: 'contactusdetails',
+            params: {
+              title: con.title,
+              url: con.url,
+            },
+          }"
+          >{{ con.title }}</router-link
+        >
+        <input type="text" name="" id="" />
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactUs",
+  data() {
+    return {
+      ContactUsList: [
+        {
+          title: "aaaaaa",
+          url: "a-a-a-a-a",
+        },
+        {
+          title: "bbbbbb",
+          url: "b-b-b-b-b",
+        },
+        {
+          title: "cccccc",
+          url: "c-c-c-c-c",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
