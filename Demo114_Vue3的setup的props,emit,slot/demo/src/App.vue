83a27f17b3a964ef2bd131bcb2297e325a902455
cheemsswap <291221550@qq.com>
2022-03-04 22:21:41 +0800

    3月4日 晚上完成demo114 了解了vue3的props传递接受 自定义方法的传递接受 插槽的传递接受

---
+++ Demo114_Vue3的setup的props,emit,slot/demo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <Demo :person="person" @sayHello="sayHello">
+    <template #abc>
+      <span>我是名称为abc插槽里面的内容</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+import Demo from "./components/Demo.vue";
+import { reactive } from "vue";
+export default {
+  name: "App",
+  components: {
+    Demo,
+  },
+  setup() {
+    let person = reactive({
+      name: "张三",
+      age: 20,
+      job: "前端开发工程师",
+    });
+    function sayHello(val) {
+      console.log(`大家好 我是App的方法 我接受到了${val}`);
+    }
+    return {
+      sayHello,
+      person,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Demo114_Vue3的setup的props,emit,slot/demo/src/components/Demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>你好</div>
+  <div>我的名字是:{{ person.name }}</div>
+  <div>我的年龄是:{{ person.age }}</div>
+  <div>我的工作是:{{ person.job }}</div>
+  <button @click="$emit('sayHello', 666)">点我触发sayHello事件</button>
+  <br />
+  <slot name="abc"></slot>
+</template>
+
+<script>
+export default {
+  name: "Demo",
+  props: ["person"], //传递过来的props
+  emits: ["sayHello"], //传递过来的自定义函数
+  slots: ["abc"], //传递过来的命名插槽
+  setup(props, content) {
+    //输出props
+    console.log("props:");
+    console.log(props);
+    //输出未被props传的 参数或者函数
+    console.log(`content->attrs:`);
+    console.log(content.attrs);
+  },
+};
+</script>
+
+<style>
+</style>
